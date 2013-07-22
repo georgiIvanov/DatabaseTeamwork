6318661e4f6a316a189ddcae54488ad4a0d3146b
Georgi <georgi.ivanovtn@gmail.com>
2013-07-22 12:13:52 +0300

    connection string test

---
+++ Teamwork/TestConnectionString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using context;
+
+namespace TestConnectionString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestContext db = new TestContext();
+
+            db.Persons.Add(new Person() { Age = 18 });
+            db.SaveChanges();
+
+            foreach (var item in db.Persons)
+            {
+                Console.WriteLine(item.Age);
+            }
+        }
+    }
+}

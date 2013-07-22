4a285226760adc5ac7ae5b5e2ddaf2e74fdac8a8
Georgi <georgi.ivanovtn@gmail.com>
2013-07-22 15:17:39 +0300

    new project

---
+++ TeamworkTasks/TeamworkTasks/SupermarketManager/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySQLStore;
+
+namespace SupermarketManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var store = new StoreModel())
+            {
+                foreach (var item in store.Vendors)
+                {
+                    Console.WriteLine(item.VendorName);
+                }
+            }
+        }
+    }
+}

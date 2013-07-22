28c9be45d09ad93781007826b391b2503f969d26
Georgi <georgi.ivanovtn@gmail.com>
2013-07-22 20:17:02 +0300

    partizan replaced

---
+++ Teamwork/SupermarketManager/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySQLStore;
+using SQLStore.Data;
+using SQLStore.Model;
+using System.Data.Entity;
+
+namespace SupermarketManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //TransferTables transferTables = new TransferTables();
+            //transferTables.TransferFromMySqlToSQLServer();
+
+            //TransferFromExcel transferExcel = new TransferFromExcel();
+            //transferExcel.ParseExcelZip("zip\\Sample-Sales-Reports.zip");
+
+            GeneratePDF pdf = new GeneratePDF();
+            pdf.CreatePDF();
+
+            using (SQLStoreDb db = new SQLStoreDb())
+            {
+                var a = from w in db.SalesOnDate
+                        select w;
+
+                //var r = from tt in db.Sales
+                //        select tt;
+            }
+
+        }
+    }
+}
